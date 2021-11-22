a3b1aec9eaf52e777258357f2fd1a7b4a0f970b8
Amber916Young <57694784+Amber916Young@users.noreply.github.com>
2021-11-22 23:46:44 +0000

    Update Socket.java
    update delete core's part. here some bugs fixed

--- src/Socket.java
+++ src/Socket.java
@@ -60,9 +60,26 @@ public class Socket {
         availableCores.remove(core);
     }
 
-    public void removeJob(final double time,
-                          final Job oneJob){
-        Core core = this.jobToCore.remove(oneJob);
+   public synchronized void  removeJob(final Job oneJob){
+        Core core = null;
+        ConcurrentHashMap<Job,Core> tempjobToCore = new ConcurrentHashMap<>();
+        for (Iterator<Map.Entry<Job, Core>> iterator =
+             this.jobToCore.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<Job,Core> entry = iterator.next();
+            Job key =entry.getKey();
+            Core value = entry.getValue();
+            this.jobToCore.remove(key);
+            if (key.equals(oneJob)){
+                core = value;
+                iterator.remove();
+                System.out.println(this.jobToCore);
+            }else {
+                tempjobToCore.put(key, value);
+            }
+        }
+        System.out.println(tempjobToCore);
+        this.jobToCore = tempjobToCore;
+
         if (core == null) {
             System.out.println("Error!Cannot find the core!");
         }else{
@@ -73,6 +90,7 @@ public class Socket {
             this.availableCores.add(core);
         }
     }
+
 
     public void process(double time)
     {
